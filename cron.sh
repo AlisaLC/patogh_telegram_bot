dbb2f1348b7a9390e2fbafed0d2dfe8646fec2e6
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2021-10-01 09:36:48 +0330

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 output=$(curl https://www.time.ir/fa/event/list/1/$(date +"%Y/%m/%d") | grep 'eventHoliday')
-[ -z "$output"] && /usr/bin/python3.9 /opt/patogh_telegram_bot-master/manage.py notifyclasses >> /var/log/notify-users.log 2>&1
+[ -z "$output" ] && /usr/bin/python3.9 /opt/patogh_telegram_bot-master/manage.py notifyclasses >> /var/log/notify-users.log 2>&1
